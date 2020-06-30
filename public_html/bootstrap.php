c3d20c2cc2358f39718bb5c0be71b4fbd98923f7
Chris Brewer <brewerc1@nku.edu>
2020-06-30 16:40:10 -0400

    Update bootstrap.php

--- public_html/bootstrap.php
+++ public_html/bootstrap.php
@@ -3,6 +3,6 @@
 /* Get the configuration array from the config file located outside the document root.
 Change the value here to your correct local path. */
 
-require_once('/local/path/to/system/config.php');
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/../system/config.php');
 
 ?>
