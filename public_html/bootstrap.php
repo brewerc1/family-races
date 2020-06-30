2ccffbbceec26486dd691c3692a03921f7fe6607
makungaj1 <jmakunga2015@gmail.com>
2020-06-30 18:52:49 -0400

    added send_email() inside bootstrap.php

--- public_html/bootstrap.php
+++ public_html/bootstrap.php
@@ -1,9 +1,62 @@
 <?php
 
 // Get the configuration array from the config file located outside the document root.
-
-
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/../system/config.php');
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+/**
+ * @param $host
+ * @param $username
+ * @param $password
+ * @param $to
+ * @param $content : may use html5 tags and should have the invite code included.
+ * @param string $subject optional
+ * @param string $from optional
+ * @param int $port optional
+ *
+ * @return bool
+ *
+ *
+ *  May have to require vendor/autoload.php
+ *
+ * @author makungaj1
+ */
+function send_email($host, $username, $password, $to, $content, $subject="", $from="", $port=587) {
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host       = $host;
+    $mail->SMTPAuth   = true;
+    $mail->Username   = $username;
+    $mail->Password   = $password;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = $port;
+
+
+    try {
+
+        if (empty($from))
+            $from = $username;
+
+        //Recipients
+        $mail->setFrom($from);
+        $mail->addAddress($to);
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $content;
+        $mail->AltBody = strip_tags($content);
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
+    }
+}
 
 ?>
