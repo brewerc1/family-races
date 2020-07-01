59a0bdd0a627a18be5f621c8bacc4801897d6c69
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2020-07-01 15:41:09 -0400

    Initial HTML for user_preferences. Fixed link to user_profile from admin/user/

--- public_html/user/settings/index.php
+++ public_html/user/settings/index.php
@@ -1,5 +1,51 @@
 <?php
-require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
-?>
+/**
+ * Page to Display User Settings
+ * 
+ * Page Description
+ */
 
-<h1>User Settings</h1>
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
+
+$uid = $_GET['u'];
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"> 
+    <title>User Settings</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">-->
+    <link href="/css/races.css" rel="stylesheet">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <nav id="main-navigation">
+        <h1>Main Navigation</h1>
+        <ul>
+            <li>Menu 1</li>
+            <li>Menu 2</li>
+            <li>Menu 3</li>
+            <li>Menu 4</li>
+            <li>Menu 5</li>
+        </ul>
+    </nav>
+    <main role="main">
+        <section id="user_settings">
+            <h1>Settings</h1>
+            <p>Sound Effects</p>
+            <p>Voiceovers</p>
+            <p>change password link</p>
+            <a href="../?u=<?php echo $uid ?>" class="button">Cancel</a>
+        </section> <!-- END id user_settings -->
+    </main>
+    
+    <footer>
+        <p>Created by students of the College of Informatics at Northern Kentucky University</p>
+    </footer>
+</body>
+</html>
