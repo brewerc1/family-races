16b05d933eea47dfa8b86711fabd91ccf366dbec
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2020-07-02 16:32:55 -0400

    testing

--- public_html/login/index.php
+++ public_html/login/index.php
@@ -4,7 +4,7 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charsetasdas ="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"> 
     <title>Skeleton HTML</title>
 
