2e9ad8f434894a09daea03c81c44c5a031c37a5d
Chris Brewer <brewerc1@nku.edu>
2020-07-06 10:18:54 -0400

    Menu cleanup
    Eliminated extraneous HTML from prior commit, fixed folder capitalization mismatch, made URLs relative instead of absolute, and eliminated "index.php" from each link.

--- public_html/template/main_menu.php
+++ public_html/template/main_menu.php
@@ -1,1 +1,10 @@
-<?php> This is where the main menu html is!<?>
+    <nav id="main-navigation">
+        <h1>Main Navigation</h1>
+        <ul>
+            <li><a href="/races/">Races</a></li>
+            <li><a href="/hof/">HOF</a></li>
+            <li><a href="/faq/">FAQ</a></li>
+            <li><a href="/profile/">Me</a></li>
+            <li><a href="/admin/">Admin</a></li>
+        </ul>
+    </nav>
