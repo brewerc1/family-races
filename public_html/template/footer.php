053ad6339fc0595714d4277edceb4599e2f3daa7
Ken <ken.ryumae@gmail.com>
2020-07-06 15:15:46 -0400

    fixup! Created separate files for the page templating and applied it to the races tab & the HOF tab

--- public_html/template/footer.php
+++ public_html/template/footer.php
@@ -4,10 +4,10 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 return <<<HTML
 
     <footer>
-            <p>Created by students of the College of Informatics at Northern Kentucky University</p>
-        </footer>
-    </body>
-    </html>
+        <p>Created by students of the College of Informatics at Northern Kentucky University</p>
+    </footer>
+</body>
+</html>
 
 HTML;
 ?>
--- public_html/template/header.php
+++ public_html/template/header.php
@@ -2,20 +2,20 @@
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 
 return <<<HTML
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"> 
-        <title>Skeleton HTML</title>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"> 
+    <title>Skeleton HTML</title>
 
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
-        <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">-->
-        <link href="/css/races.css" rel="stylesheet">
-        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    </head>
-    <body>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="/css/races.css" rel="stylesheet">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
     
 HTML;
 ?>
