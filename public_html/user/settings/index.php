d6522b3ad0a23d3d0cdb56734e1949d9226f948f
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2020-07-07 12:36:39 -0400

    User Preferences page now interacts with DB Reads  to display settings status correctly On successful update, requeries the user record to update -Ensures  is always up to date with user DB record

--- public_html/user/settings/index.php
+++ public_html/user/settings/index.php
@@ -2,7 +2,8 @@
 /**
  * Page to Display User Settings
  * 
- * Page Description
+ * Page displays the current user's settings via checkbox.
+ * 
  */
 
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
@@ -11,7 +12,7 @@ $page_title = "User Profile";
 $javascript = '';
 
 // Authentication System
-ob_start();
+ob_start('template');
 session_start();
 
 if (!isset($_SESSION["id"]) || $_SESSION["id"] == 0)
@@ -20,6 +21,38 @@ if (!isset($_SESSION["id"]) || $_SESSION["id"] == 0)
 // Get UID
 $uid = $_SESSION['id'];
 
+// Check if "save" button was clicked
+if(isset($_POST['save_button'])){
+
+    // If the sound_fx variable was not sent, or is not what is expected, set to 0
+    if(!isset($_POST['sound_fx']) || $_POST['sound_fx'] != 'on'){
+        $sound_fx_value = 0;
+    } else {
+        $sound_fx_value = 1;
+    }
+    // Same process for voiceovers. Any future 'boolean' settings follow this logic
+    if(!isset($_POST['voiceovers']) || $_POST['voiceovers'] != 'on'){
+        $voiceovers_value = 0;
+    } else {
+        $voiceovers_value = 1;
+    }
+
+    // PDO to update the DB 
+    $update_preferences_sql = "UPDATE user SET sound_fx = :sound_fx_value, voiceovers = :voiceovers_value  WHERE id = :uid";
+    $update_preferences_result = $pdo->prepare($update_preferences_sql);
+    $update_preferences_result->execute(['sound_fx_value' => $sound_fx_value, 'voiceovers_value' => $voiceovers_value, 'uid' => $uid]);
+    
+    //requery DB to update $_SESSION. Ensures current settings are always displayed
+    if ($update_preferences_result){    
+    $update_session_sql = "SELECT sound_fx, voiceovers FROM user WHERE id = :uid";
+    $update_session_result = $pdo->prepare($update_session_sql);
+    $update_session_result->execute(['uid' => $uid]);
+    $row = $update_session_result->fetch();
+    
+    $_SESSION['sound_fx'] = $row['sound_fx'];
+    $_SESSION['voiceovers'] = $row['voiceovers'];
+    }
+}
 ?>
 {header}
 {main_nav}
@@ -27,49 +60,22 @@ $uid = $_SESSION['id'];
         <section id="user_settings">
             <h1>Settings</h1>
             
-            <p><label>
-                <input type="checkbox" <?php if($_SESSION["sound_fx"]){echo 'checked';} ?> onclick="handleClick(this);">
-                Sound Effects
-            </label></p>
+            <form action="./" method="post">
+                <p><label>
+                    <input type="checkbox" name="sound_fx" <?php if($_SESSION['sound_fx'] == 1){echo 'checked';} ?>>
+                    Sound Effects
+                </label></p>
 
-            <p><label>
-                <input type="checkbox" <?php if($_SESSION["voiceovers"]){echo 'checked';} ?> onclick="handleClick(this);">
-                Voiceovers
-            </label></P>
-            
-            <a href="../../password/reset.php" class="button">Change Password</a>
-            <a href="../?u=<?php echo $uid ?>" class="button">Cancel</a>
+                <p><label>
+                    <input type="checkbox" name="voiceovers" <?php if($_SESSION['voiceovers'] == 1){echo 'checked';} ?>>
+                    Voiceovers
+                </label></P>
+                <input type="submit" value="Save" name="save_button">
+            </form>
+            <p><a href="../../password/reset.php" >Change Password</a></p>
+            <p><a href="../?u=<?php echo $uid ?>" >Cancel</a></p>
             
         </section> <!-- END id user_settings -->
-        <section id="testing_area">
-        <?php 
-        // Check if "save" button was clicked
-        if(isset($_POST['save_button'])){
-            var_dump($_POST);
-            
-            //fix: 
-            if(!$_POST['sound_fx']){ $sound_fx_value = 0; } 
-            // else if ($_POST[]){ $sound_fx_value = 1; }
-
-            // from Chris: if(!isset($_POST['sound_fx']) || $_POST['sound_fx'] != '1'){set to 0}
-            //if(!isset($_POST['sound_fx']) || $_POST['sound_fx'] != '1'){set to 0}
-
-            
-            //do the same for voiceovers
-            if(!$_POST['voiceovers']){ $voiceover_value = 0; } else { $voiceovers_value = 1; }
-
-
-            $voiceovers_value = $_POST['voiceovers'];
-
-            $update_preferences_sql = "UPDATE user SET sound_fx = :sound_fx_value, voiceovers = :voiceovers_value  WHERE id = :uid;";
-            $update_preferences_result = $pdo->prepare($update_preferences_sql);
-            $update_preferences_result->execute(['sound_fx_value' => $sound_fx_value, 'voiceovers_value' => $voiceovers_value, 'uid' => $uid]);
-            $row = $user_settings_result->fetch();
-            
-
-        }
-        ?>
-        </section>
     </main>
 {footer}
 <?php ob_end_flush(); ?>
