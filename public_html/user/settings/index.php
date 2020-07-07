71f10adb6e05a98d89f9205d20a54f70ec9a855b
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2020-07-07 10:18:29 -0400

    User preferences page now uses templating engine

--- public_html/user/settings/index.php
+++ public_html/user/settings/index.php
@@ -7,6 +7,9 @@
 
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 
+$page_title = "User Profile";
+$javascript = '';
+
 // Authentication System
 ob_start();
 session_start();
@@ -17,53 +20,9 @@ if (!isset($_SESSION["id"]) || $_SESSION["id"] == 0)
 // Get UID
 $uid = $_SESSION['id'];
 
-// TODO: create SQL to update preferences
-
-
-
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
-    <title>Skeleton HTML</title>
-
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link href="/css/races.css" rel="stylesheet">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <style>
-        nav#main-navigation li {
-            display: inline-block;
-            width: 18%;
-        }
-        nav#main-navigation ul {
-            margin:0;
-            padding:0;
-        }
-    </style>
-</head>
-<body>
-<!--The main navigation menu to be displayed on most pages. Not all links work yet.-->
-<nav id="main-navigation">
-    <h1>Main Navigation</h1>
-    <ul>
-        <li><a href="http://localhost/races">Races</a></li>
-        <li><a href="http://localhost/HOF/">HOF</a></li>
-        <li><a href="http://localhost/faq/">FAQ</a></li>
-        <li><a href="http://localhost/user/">Me</a></li>
-        <?php
-        if ($_SESSION['admin']) {
-            echo <<< ADMIN
-<li><a href= "http://localhost/admin/">Admin</a></li>
-ADMIN;
-        }
-        ?>
-        <li><a href="http://localhost/logout">Log out</a></li>
-    </ul>
-</nav>
+{header}
+{main_nav}
     <main role="main">
         <section id="user_settings">
             <h1>Settings</h1>
@@ -112,9 +71,5 @@ ADMIN;
         ?>
         </section>
     </main>
-
-<footer>
-    <p>Created by students of the College of Informatics at Northern Kentucky University</p>
-</footer>
-</body>
-</html>
+{footer}
+<?php ob_end_flush(); ?>
