b340fa8095106f92217e08bda4954f9a804039eb
Liz Schnuck <sketche@nku.edu>
2020-07-06 22:23:51 -0400

    Changes to be committed: links fixed, title changed

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -6,7 +6,7 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"> 
-    <title>Admin html</title>
+    <title>Admin</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">-->
@@ -25,25 +25,25 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
     </style>
 </head>
 <body>
-    <!--The main navigation menu to be displayed on most pages. Not all links work yet.-->
+    
     <nav id="main-navigation">
         <h1>Main Navigation</h1>
         <ul>
-            <li><a href="/races/index.php">Races</a></li>
-            <li><a href="/hof/index.php">HOF</a></li>
-            <li><a href="/faq/index.php">FAQ</a></li>
-            <li><a href="/profile/index.php">Me</a></li>
-            <li><a href="/admin/index.php">Admin</a></li>
+            <li><a href="/races/">Races</a></li>
+            <li><a href="/hof/">HOF</a></li>
+            <li><a href="/faq/">FAQ</a></li>
+            <li><a href="/user/">Me</a></li>
+            <li><a href="/admin/">Admin</a></li>
         </ul>
     </nav>
     <main role="main">
         <section>
-            <h1>The Stables</h1>
+            <h1>Admin</h1>
             <ul >
                 <li><a href="link to current event"> Current Event </a></li>
-                <li><a href="/admin/events">Event & Race Managment</a></li>
+                <li><a href="/events/">Event & Race Managment</a></li>
                 <li><a href="/user/">User Management</a></li>
-                <li><a href="/admin/settings">Site Settings</a></li>
+                <li><a href="/settings/">Site Settings</a></li>
             </ul>
         </section>
      
