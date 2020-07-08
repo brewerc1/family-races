798f863c88e8ec17c3f8cb5fb23b160f33c2870a
makungaj1 <jmakunga2015@gmail.com>
2020-07-07 20:59:35 -0400

    Added the unique code in the body of the email. Added Bootstrap and Secure Notification in the user invite page admin/users/index

--- public_html/admin/users/invite_user.php
+++ public_html/admin/users/invite_user.php
@@ -5,14 +5,14 @@ if (isset($_POST["invite"])) {
     $email = trim($_POST["email"]);
 
     if (!filter_var(trim($email), FILTER_VALIDATE_EMAIL)) {
-        header("Location: ./?message=Incorrect Email");
+        header("Location: ./?message=1&alt=1");
     } else {
         $query = "SELECT email FROM user WHERE email = :email";
         $emails = $pdo->prepare($query);
         $emails->execute(['email' => $email]);
 
         if ($emails->rowCount() != 0) {
-            header("Location: ./?message=User already invited");
+            header("Location: ./?message=2&alt=1");
         } else {
             $query = "SELECT invite_email_subject, invite_email_body, email_server, email_server_port, email_server_account, email_server_password, email_from_name, email_from_address FROM site_settings";
             $email_arguments = $pdo->prepare($query);
@@ -20,7 +20,7 @@ if (isset($_POST["invite"])) {
             $row = $email_arguments->fetch();
 
             if ($email_arguments->rowCount() == 0) {
-                header("Location: ./?message=DB is empty");
+                header("Location: ./?message=3&alt=1");
             } else {
 
                 if (empty($row["invite_email_subject"]) || empty($row["invite_email_body"]) ||
@@ -28,35 +28,35 @@ if (isset($_POST["invite"])) {
                     empty($row["email_server_account"]) || empty($row["email_server_password"]) ||
                     empty($row["email_from_name"]) || empty($row["email_from_address"])) {
 
-                    header("Location: ./?message=DB is empty");
+                    header("Location: ./?message=3&alt=2");
 
                 } else {
                     try {
                         $unique_code = generateCode();
                     } catch (Exception $e) {
-                        header("Location: ./?message=Fails to generate Code");
+                        header("Location: ./?message=4&alt=2");
                         exit; //
                     }
 
                     // write to the db
                     $sql = "INSERT INTO user (email, invite_code) VALUES (?,?)";
                     if (!$pdo->prepare($sql)->execute([$email, $unique_code])) {
-                        header("Location: ./?message=Couldn't write to DB");
+                        header("Location: ./?message=5&alt=2");
 
                     } else {
                         //$row = $email_arguments->fetch();
                         // send invite
                         $host = $_SERVER['SERVER_NAME'];
-                        $invite_email_body = "<p>" . $row["invite_email_body"] . " <a href=\"http://$host/onboarding/?email=$email&code=$unique_code\">family race</a> </p>";
+                        $invite_email_body = "<p>" . $row["invite_email_body"] . " <ul> <li>Code: $code</li> <li><a href=\"http://$host/onboarding/?email=$email&code=$unique_code\">family race</a></li> </ul> </p>";
 
                         if (!sendEmail($row["email_server"], $row["email_server_account"],
                             $row["email_server_password"], $row["email_server_port"], $row["email_from_name"],
                             $row["email_from_address"], $row["invite_email_subject"], $invite_email_body, $email)) {
 
-                            header("Location: ./?message=Invite not sent");
+                            header("Location: ./?message=6&alt=2");
 
                         } else {
-                            header("Location: ./?message=Invite sent");
+                            header("Location: ./?message=7&alt=1");
                         }
 
                     }
