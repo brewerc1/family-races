a464db0615304e3e3d00e2cdf5ce186749a9b3a0
Chris Brewer <brewerc1@nku.edu>
2020-07-13 11:27:30 -0400

    Modal dialog and logout
    Added Bootstrap4 Modal Dialog, which can be invoked thusly:
    
    <a class="nav-link" href="#"
    data-toggle="modal"
    data-target="#mainModal"
    data-title="Logout"
    data-message="<p>Are you sure you want to log out?</p>"
    data-button-primary-text="Logout"
    data-button-primary-action="window.location.href='/logout/'"
    data-button-secondary-text="Cancel"
    data-button-secondary-action=""
    >Logout</a>
    
    where:
    
    data-toggle="modal" invokes the Bootstrap 4 modal. The value "modal" is mandatory.
    data-target="#mainModal" sets the target of the modal HTML
    data-title="Logout" sets the title displayed in the modal window
    data-message="Are you sure you want to log out?" is the text displayed in the modal window (HTML is okay)
    data-button-primary-text="Logout" sets the text of the primary button
    data-button-primary-action="window.location.href='/logout/'" sets a javascript onclick action (mandatory)
    data-button-secondary-text="Cancel" sets the text of the secondary button
    data-button-secondary-action=""  sets a javascript onclick action for the secondary button (optional)

--- public_html/template/footer.php
+++ public_html/template/footer.php
@@ -1,7 +1,7 @@
 <?php
 return <<< HTML
     <footer>
-        <p>Created by students of the College of Informatics at Northern Kentucky University</p>
+        
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
