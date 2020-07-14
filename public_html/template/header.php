6e97ccd85513c7a1713a3d392fa687536db7ca04
Chris Brewer <brewerc1@nku.edu>
2020-07-14 10:09:02 -0400

    Update header.php
    Move modal script into head with document.ready().
    
    Eliminate Bootstrap4 Toggle library to instead use native Bootstrap 4 switches.

--- public_html/template/header.php
+++ public_html/template/header.php
@@ -10,20 +10,44 @@ return <<< HTML
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="/css/races.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    
-    <!-- Bootstrap4 Toggle: https://gitbrent.github.io/bootstrap4-toggle/ -->
-    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
     <script>
         $( document ).ready(function() {
             $javascript
+
             var path_id = $(location).attr('pathname').split('/')[1];
             $("#" + path_id).addClass("active");
+
+            $('#mainModal').on('show.bs.modal', function (event) {
+                // Link or Button that triggered the modal
+                var button = $(event.relatedTarget);
+                
+                // Extract info from data-* attributes
+                var title = button.data('title');
+                var message = button.data('message');
+                var button_primary_text = button.data('button-primary-text');
+                var button_primary_action = button.data('button-primary-action');
+                var button_secondary_text = button.data('button-secondary-text');
+                var button_secondary_action = button.data('button-secondary-action');
+
+                var modal = $(this);
+                modal.find('.modal-title').text(title);
+                modal.find('#message').html(message);
+                modal.find('.btn.btn-primary').html(button_primary_text);
+                modal.find('.btn.btn-primary').attr("onclick",button_primary_action);
+                modal.find('.btn.btn-secondary').html(button_secondary_text);
+
+                // Close main_nav when modal is active
+                var trigger_id =  $('#main-navigation').attr('data-target');
+                $(trigger_id).toggleClass("collapsed");
+            });
+
         });
     </script>
+
 </head>
 <body>
 
@@ -48,29 +72,4 @@ return <<< HTML
   </div>
 </div>
 
-<script>
-$('#mainModal').on('show.bs.modal', function (event) {
-    // Link or Button that triggered the modal
-    var button = $(event.relatedTarget);
-    
-    // Extract info from data-* attributes
-    var title = button.data('title');
-    var message = button.data('message');
-    var button_primary_text = button.data('button-primary-text');
-    var button_primary_action = button.data('button-primary-action');
-    var button_secondary_text = button.data('button-secondary-text');
-    var button_secondary_action = button.data('button-secondary-action');
-
-    var modal = $(this);
-    modal.find('.modal-title').text(title);
-    modal.find('#message').html(message);
-    modal.find('.btn.btn-primary').html(button_primary_text);
-    modal.find('.btn.btn-primary').attr("onclick",button_primary_action);
-    modal.find('.btn.btn-secondary').html(button_secondary_text);
-
-    // Close main_nav when modal is active
-    $('#main-navigation button.navbar-toggler').toggleClass("collapsed");
-})
-</script>
 HTML;
-?>
