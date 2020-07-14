282ab22e4fad00fdf6d2d28cfd1f1b3242f1f8ca
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2020-07-14 10:53:51 -0400

    HTML and PHP updates All displayed fields are now forms State field is a select menu using defined 'state' array SQL written, testing to follow

--- public_html/user/edit/index.php
+++ public_html/user/edit/index.php
@@ -30,6 +30,7 @@ if (!isset($_SESSION["id"])){
 }
 
 // logged in user
+$user_id = $_SESSION['id'];
 $first_name = $_SESSION['first_name'];
 $last_name = $_SESSION['last_name'];
 $full_name = $_SESSION['first_name'].' '.$_SESSION['last_name'];
@@ -38,23 +39,167 @@ $motto = $_SESSION['motto'];
 $email = $_SESSION['email'];
 $city = $_SESSION['city'];
 $state = $_SESSION['state'];
+
+// State Select Array
+$state_array = array(
+
+    "AK" => "Alaska",
+    "AL" => "Alabama",
+    "AR" => "Arkansas",
+    "AZ" => "Arizona",
+    "CA" => "California",
+    "CO" => "Colorado",
+    "CT" => "Connecticut",
+    "DC" => "District of Columbia",
+    "DE" => "Delaware",
+    "FL" => "Florida",
+    "GA" => "Georgia",
+    "HI" => "Hawaii",
+    "IA" => "Iowa",
+    "ID" => "Idaho",
+     "IL" => "Illinois",
+    "IN" => "Indiana",
+    "KS" => "Kansas",
+    "KY" => "Kentucky",
+    "LA" => "Louisiana",
+    "MA" => "Massachusetts",
+    "MD" => "Maryland",
+    "ME" => "Maine",
+    "MI" => "Michigan",
+    "MN" => "Minnesota",
+    "MO" => "Missouri",
+    "MS" => "Mississippi",
+    "MT" => "Montana",
+    "NC" => "North Carolina",
+    "ND" => "North Dakota",
+    "NE" => "Nebraska",
+    "NH" => "New Hampshire",
+    "NJ" => "New Jersey",
+    "NM" => "New Mexico",
+    "NV" => "Nevada",
+    "NY" => "New York",
+    "OH" => "Ohio",
+    "OK" => "Oklahoma",
+    "OR" => "Oregon",
+    "PA" => "Pennsylvania",
+    "PR" => "Puerto Rico",
+    "RI" => "Rhode Island",
+    "SC" => "South Carolina",
+    "SD" => "South Dakota",
+    "TN" => "Tennessee",
+    "TX" => "Texas",
+    "UT" => "Utah",
+    "VA" => "Virginia",
+    "VT" => "Vermont",
+    "WA" => "Washington",
+    "WI" => "Wisconsin",
+    "WV" => "West Virginia",
+    "WY" => "Wyoming"
+);
+
+// Check if "save" button was clicked
+if(isset($_POST['save_button'])){
+    // User Photo Upload
+
+
+
+
+    // First Name Text
+    if(!isset($_POST['first_name'])){
+        $first_name_value = $_SESSION['first_name'];
+    } else {
+        $first_name_value = htmlentities($_POST['first_name']);
+    }
+
+    // Last Name Text
+    if(!isset($_POST['last_name'])){
+       $last_name_value = $_SESSION['last_name'];
+    } else {
+       $last_name_value = htmlentities($_POST['last_name']);
+    }
+
+    // Motto Text
+    if(!isset($_POST['motto'])){
+        $motto_value = $_SESSION['motto'];
+    } else {
+        $motto_value = htmlentities($_POST['motto']);
+    }
+
+    // Email Text
+    if(!isset($_POST['email'])){
+        $email_value = $_SESSION['email'];
+    } else {
+        $email_value = htmlentities($_POST['email']);
+    }
+
+    // City Text
+    if(!isset($_POST['city'])){
+        $city_value = $_SESSION['city'];
+    } else {
+        $city_value = htmlentities($_POST['city']);
+    }
+
+    // State Text
+    if(!isset($_POST['state'])){
+        $state_value = $_SESSION['state'];
+    } else {
+        $state_value = htmlentities($_POST['state']);
+    }
+
+
+
+    // PDO to update the DB 
+    $update_preferences_sql = 
     
+    "UPDATE user SET 
+    photo = :photo_value, first_name = :first_name_value, last_name = :last_name_value,
+    motto = :motto_value, email = :email_value, city = :city_value, state = :state_value
+    WHERE id = :user_id";
+
+    $update_preferences_result = $pdo->prepare($update_preferences_sql);
+    $update_preferences_result->execute(['photo' => $photo_value, 'first_name' => $first_name_value, 'last_name' => $last_name_value,
+    'motto' => $motto_value, 'email' => $email_value, 'city' => $city_value, 'state' => $state_value, 'user_id' => $user_id]);
+    
+    //requery DB to update $_SESSION. Ensures current settings are always displayed
+    if ($update_preferences_result){    
+    $update_session_sql = 
+    "SELECT photo, first_name, last_name, motto, email, city, state, id
+    FROM user WHERE id = :user_id";
+    $update_session_result = $pdo->prepare($update_session_sql);
+    $update_session_result->execute(['id' => $user_id]);
+    $row = $update_session_result->fetch();
+    
+    $_SESSION['photo'] = $row['photo'];
+    $_SESSION['first_name'] = $row['first_name'];
+    $_SESSION['last_name'] = $row['last_name'];
+    $_SESSION['motto'] = $row['motto'];
+    $_SESSION['email'] = $row['email'];
+    $_SESSION['city'] = $row['city'];
+    $_SESSION['state'] = $row['state'];
+    
+    }
+}
 
 ?>
 {header}
 {main_nav}
     <main role="main">
         <div class="container">
-            <form>
+            <form action="./index.php" method="post">
                 <section id="user_head">
-                    <div class="form-group" id="profile_photo">
-                    <label for="profile_photo">
-                        <img class="img-fluid" src="<?php echo $photo ?>" alt="User Photo"/></label>
-                        <input type="file" class="form-control-file" id="profile_photo">
-                    </div>
-                    <div class="form-group" id="user_name"><?php  ?>
-                    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="<?php echo $_SESSION['first_name'] ?>">
-                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="<?php echo $_SESSION['last_name'] ?>">
+                    <div class="form-group row">
+                        <div class="col-auto" id="profile_photo">
+                            <label for="profile_photo">
+                                <img class="img-fluid" src="<?php echo $photo ?>" alt="User Photo"/>
+                            </label>
+                            <input type="file" class="form-control-file" id="profile_photo">
+                        </div>
+                        <div class="col-auto" id="user_name">
+                            <label for="first_name" class="col-form-label sr-only">First Name </label>
+                            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="<?php echo $_SESSION['first_name'] ?>">
+                            <label for="last_name" class="col-form-label sr-only">First Name </label>
+                            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="<?php echo $_SESSION['last_name'] ?>">
+                        </div>
                     </div>
                     <!-- Links not displayed if "logged in" == "displayed" -->  
                     <?php
@@ -70,27 +215,52 @@ LINKS;
                 </section> <!-- END user_head -->
 
                 <section id="user_meta">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <th>Motto:</th>
-                                <td><input type="text" class="form-control" id="motto" name="motto" placeholder="<?php echo $_SESSION['motto'] ?>"></td>
-                            </tr>
-                            <tr>
-                                <th>Email:</th>
-                                <td><input type="text" class="form-control" id="email" name="email" placeholder="<?php echo $_SESSION['email'] ?>"></td>
-                            </tr>
-                            <tr>
-                                <th>City:</th>
-                                <td><input type="text" class="form-control" id="city" name="city" placeholder="<?php echo $_SESSION['city'] ?>"></td>
-                            </tr>
-                            <tr>
-                                <th>State:</th>
-                                <td><input type="text" class="form-control" id="state" name="state" placeholder="<?php echo $_SESSION['state'] ?>"></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <!-- trying to just use a form row to mimic the displayed table  -->
+                    <div class="form-group row">
+                        <div class="col-auto">
+                            <label for="motto" class="col-form-label" >Motto:</label> 
+                            <input type="text" class="form-control" id="motto" name="motto" placeholder="<?php echo $_SESSION['motto'] ?>">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-auto">
+                            <label for="email" class="col-form-label" >Email:</label> 
+                            <input type="text" class="form-control" id="email" name="email" placeholder="<?php echo $_SESSION['email'] ?>">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-auto">
+                            <label for="city" class="col-form-label" >City:</label> 
+                            <input type="text" class="form-control" id="city" name="city" placeholder="<?php echo $_SESSION['city'] ?>">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-auto">
+                            <label for="state" class="col-form-label" >State:</label> 
+                            <select class="form-control" id="state" name="state">
+                                <?php
+                                foreach ($state_array as $key => $value) {
+                                    if($_SESSION['state'] == $key){
+                                        $state_selected_tag = 'selected';
+                                    } else {
+                                        $state_selected_tag = '';
+                                    }
+echo <<<ENDOPTION
+                                    <option value="$value" $state_selected_tag>$key</option>
+ENDOPTION;
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+
                 </section><!-- END user_meta -->
+
+                <button type="submit" class="btn btn-primary" name="save_button">Save</button> <a href="../index.php" >Cancel</a>
+           
             </form>
             <section id="user_records">
                 <h1>Keenland Records</h1>
