78b3e10969c9a5759d2088cb5e9253ee16b8a3a3
Liz Schnuck <sketche@nku.edu>
2020-07-13 21:46:00 -0400

    Changes to be committed: Basic html laid out

--- public_html/hof/index.php
+++ public_html/hof/index.php
@@ -8,7 +8,17 @@ ob_start('template');
 ?>
 {header}
 {main_nav}
-    <h1>HOF Page</h1>
-    
+    <main role="main">
+    <h1>Hall of Fame</h1>
+    <section>
+        <h2>"Most recent event" Champion</h2>
+        <a href="/.user/"><img src="/images/no-user-image.jpg" alt="Photo of HOF winner" width="100" height="100"></a>
+        <p>champion name  purse</p>
+    </section>
+    <section>
+        <h2>Prior Champions</h2>
+        <!--loop to show prior winners events, pic, name and purse-->
+    </section>
+
 {footer}
 <?php ob_end_flush(); ?>
