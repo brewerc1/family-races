3658d977562beaac1e95f5ad120853ae12a31e29
Josh Hannon <joshhannon15@gmail.com>
2020-07-14 16:33:13 -0400

    Update step2.php

--- public_html/onboarding/step2.php
+++ public_html/onboarding/step2.php
@@ -1,5 +1,28 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 ?>
+{header}
+{main_nav}
+<h1>Your Profile</h1>
 
-<h1>Create Account: Step 2</h1>
+
+<form>
+                <div class="form-group">
+                    <input  type="text" class= "form-control" id="FN" name="FN"  placeholder="First Name"></input>
+                </div>
+                <div class="form-group">
+                    <input  type="text" class= "form-control" id="LN" name="LN"  placeholder="Last Name"></input>
+                </div>
+                <div class="form-group">
+                    <input  type="text"  class= "form-control" id="city" name="city" placeholder="City"></input>
+                </div>
+                <div class="form-group">
+                    <input  type="text" class= "form-control" id="state" name="state" placeholder="State"></input>
+                </div>
+                <div class="form-group">
+                    <input  type="text" class= "form-control" id="motto" name="motto" placeholder="Motto"></input>
+                </div>
+                    <input type="submit" name="next-btn" value="Next"></input>
+    </form>
+    {footer}
+<?php ob_end_flush(); ?>
