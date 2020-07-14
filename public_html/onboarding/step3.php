3ed1691e24489328e55bb3df3ff8b386f613a705
Josh Hannon <joshhannon15@gmail.com>
2020-07-14 16:34:04 -0400

    Update step3.php

--- public_html/onboarding/step3.php
+++ public_html/onboarding/step3.php
@@ -1,5 +1,16 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 ?>
-
-<h1>Create Account: Step 3</h1>
+{header}
+{main_nav}
+<h1>Your Photo</h1>
+        <div>
+            <img src="no-user-image.jpg">
+        </div>
+    <form>
+        <div class="form-group">
+            <input type="file" class="form-control-file" id="photoUpload" name="photoUpload" placeholder="Add a Profile Photo">
+        </div>
+    </form>
+{footer}
+<?php ob_end_flush(); ?>
