36f03cd8c96be336bd69684f39e66f2d7ec77f69
Josh Hannon <joshhannon15@gmail.com>
2020-07-21 15:43:27 -0400

    Update step3.php

--- public_html/onboarding/step3.php
+++ public_html/onboarding/step3.php
@@ -1,5 +1,33 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
+
+// turn on output buffering
+ob_start('template');
+
+// start a session
+session_start();
+if (isset($_POST['skip-btn'])) {
+    Header('Location:/onboarding/step3.php');
+}
+if (isset($_POST['submit-btn'])) {
+    //User Photo Upload
+    //TODO: Impliment Cropper or similar plugin
+    $photo_value = $_SESSION['photo'];
+    if ($_FILES['profile_photo']['error'] == 0 && isset($_FILES['profile_photo'])) {
+        $name = $_FILES['profile_photo']['name'];
+        $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/uploads/";
+        $target_file = $target_dir . basename($_FILES['profile_photo']['name']);
+        $image_file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        $extensions_arr = array("jpg","jpeg","png","gif");
+
+        if(in_array($image_file_type, $extensions_arr)){
+            $unlink_result=unlink($_SERVER['DOCUMENT_ROOT'] . $photo_value);
+            $debug = debug($unlink_result); 
+            if(move_uploaded_file($_FILES['profile_photo']['tmp_name'], $target_dir . $user_id .".". $image_file_type))
+            $photo_value = "/uploads/$user_id.$image_file_type"; 
+        }
+    }
+}
 ?>
 {header}
 {main_nav}
@@ -10,6 +38,10 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
     <form>
         <div class="form-group">
             <input type="file" class="form-control-file" id="photoUpload" name="photoUpload" placeholder="Add a Profile Photo">
+        </div>
+        <input type ="submit" class="btn btn-primary" name="sumbit-btn" value="Upload">
+        <div>
+            <input type ="submit" class="btn btn-primary" name="skip-btn" value="Skip">
         </div>
     </form>
 {footer}
