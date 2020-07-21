a2b9258ac00102612ba8a2413749bbaceb12d9c4
Josh Hannon <joshhannon15@gmail.com>
2020-07-21 17:59:41 -0400

    Update index.php

--- public_html/onboarding/index.php
+++ public_html/onboarding/index.php
@@ -3,29 +3,29 @@
 
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 
-// Authorization System
-// Secure resource: invited user with email and valid code only
-//if (!isset($_GET["email"]) && !isset($_GET["code"])) {
-    //header("HTTP/1.1 401 Unauthorized");
+ //Authorization System
+//Secure resource: invited user with email and valid code only
+if (!isset($_GET["email"]) && !isset($_GET["code"])) {
+    header("HTTP/1.1 401 Unauthorized");
     // An error page
-    //header("Location: error401.php");
-    //exit;
-//}
+    header("Location: error401.php");
+    exit;
+}
 
-//$email = $_GET["email"];
-//$code = $_GET["code"];
+$email = $_GET["email"];
+$code = $_GET["code"];
 
-//$query = "SELECT * FROM user WHERE invite_code = :invite_code";
+$query = "SELECT * FROM user WHERE invite_code = :invite_code";
 
-//$invite_code = $pdo->prepare($query);
-//$invite_code->execute(['invite_code' => $code]);
+$invite_code = $pdo->prepare($query);
+$invite_code->execute(['invite_code' => $code]);
 
-//if ($invite_code->rowCount() != 1) {
-    //header("HTTP/1.1 401 Unauthorized");
+if ($invite_code->rowCount() != 1) {
+    header("HTTP/1.1 401 Unauthorized");
     // An error page
-    //header("Location: error401.php");
-    //exit;
-//}
+    header("Location: error401.php");
+    exit;
+}
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
  
 // turn on output buffering
