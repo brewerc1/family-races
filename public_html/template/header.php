dc59ed821bd775565d40267286f6455f66a68c65
RobRuwe <54559269+RobRuwe@users.noreply.github.com>
2020-07-21 12:22:01 -0400

    added Croppie support to header.php

--- public_html/template/header.php
+++ public_html/template/header.php
@@ -10,9 +10,11 @@ return <<< HTML
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="/css/races.css" rel="stylesheet">
+    <link href="/library/croppie/node_modules/croppie/croppie.css" rel="stylesheet"/>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/library/jquery.multifield.min.js"></script>
+    <script src="/library/croppie/node_modules/croppie/croppie.js"></script>
 
     <script>
         $( document ).ready(function() {
