c9eabe4b11a0221b4d3337d7f10ee79c79cc9734
Josh Hannon <joshhannon15@gmail.com>
2020-07-21 09:52:16 -0400

    Update index.php

--- public_html/onboarding/index.php
+++ public_html/onboarding/index.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-if I get post, then:
-    a) make sure fields are filled in and good.
-    b) query db select * from users where email=post[emakil] and code=post[code];
-    c) If 0 result, then they don't match. So handle error (see login/index.php for messages for how errors work). 
-    d) If I get single row result, then they match and continue processing.
-    e) set session vars for id=row_result[id], email=row_result[email], session[photo]=/images/no-user-image.jpg, etc.
-    f) update row with hashed password and photo to session[photo] and set invite_code to null
-    g) forward (header) to step2
 
-
-
-
-*/
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 
 // Authorization System
@@ -39,7 +26,111 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
     //header("Location: error401.php");
     //exit;
 //}
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
+ 
+// turn on output buffering
+ob_start('template');
+ 
+// start a session
+session_start();
+ 
+// Test for authorized user
+//if (!isset($_SESSION["id"])) {
+    //header("Location: /login/");
+    //exit;
+//} elseif ($_SESSION["id"] == 0) {
+    //header("Location: /login/");
+    //exit;
+//}
+ 
+// Set the page title for the template
+$page_title = "Login";
+ 
+// Include the race picker javascript
+$javascript = '';
+ 
+// Get UID
+//$uid = $_SESSION['id'];
+ 
+///// DEBUG
+$debug = debug();
+///// end DEBUG
+<?php
+$notification = array();
+// Check if the CreateAccount button is clicked
+if (isset($_POST['createAccount-btn'])) {
+    //$email = trim($_POST ['email']);
+    //$code = trim($_POST ['code']);
+//Validation Email filed filled and email exist
+    if ((empty($_POST['email'])) && (!filter_var($email, FILTER_VALIDATE_EMAIL))) {
+        $notification ['email'] = 'Email is Required and must be valid';
+        exit;
+    }
+//Check if email exist
+    //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       // $errors ['email'] = 'Email address is invalid';
+   // }
+//Validation Code
+    if(empty($_POST['code'])){
+        $notification ['code'] = 'Code Required';
+        exit;
+    }
+//Validation Password
+    if(empty($_POST['password'])) {
+        $notification ['password'] = 'Password Required';
+        exit;
+    }
+//Check if passwords and confirmation match
+    if($_POST['password'] !== $_POST['confirmPassword']) {
+        $notification ['password'] = 'The two passwords do not match';
+        exit;
+    }
+    $password= trim($_POST['password']);
+    $email= trim($_POST['email']);
+    $code = trim($_POST['code']);
+    $photo = '/images/no-user-image.jpg';
+    //Selecting code and email from users to see if it's same as $code and $email
+        $sqlcheck = "SELECT email, invite_code, id FROM user WHERE email = :email and invite_code = :code";
+        $stmt1 = $pdo->prepare($sqlcheck);
+        $stmt1->execute(['email' => $email, 'code' => $code]);
+        $stmt1_Result=$stmt1->rowCount();
+        $row = $stmt1->fetch();
+        $user_id = $row['id'];
+        //echo var_dump($stmt);
+        //exit;
+        if($stmt1) {
+        $sql = "UPDATE user 
+                SET password= :password, photo=:photo, invite_code = NULL
+                WHERE invite_code = :code AND email = :email";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['password'=> $password, 'photo' => $photo, 'code' => $code, 'email' => $email]);
 
+        }
+        else{
+            $notification ['db_error'] = "The code and or email you provided do not match the values in the database. Please Try Again!";
+
+        }
+
+
+    if ($stmt) {
+        //Update Session Variables
+        $updateSession ="SELECT * FROM user WHERE id = :user_id";
+        $updateSessionResult = $pdo->prepare($updateSession);
+        $updateSessionResult->execute(['user_id' => $user_id]);
+        $row= $updateSessionResult->fetch();
+        $_SESSION ['id'] = $row['id'];
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['password'] = $row['password'];
+        $_SESSION['photo'] = $row['photo'];
+        header('Location:/onboarding/step2.php/');
+
+    } else {
+        //$errors['db_error'] = "Database error: Failed to Register";
+    }
+    }
+
+
+?>
 ?>
 {header}
 {main_nav}
@@ -101,18 +192,21 @@ if (isset($_POST['createAccount-btn'])) {
     $code = trim($_POST['code']);
     $photo = '/images/no-user-image.jpg';
     //Selecting code and email from users to see if it's same as $code and $email
-        $sqlcheck = "SELECT email, invite_code FROM users WHERE email = :email and invite_code = :code";
+        $sqlcheck = "SELECT email, invite_code, id FROM user WHERE email = :email and invite_code = :code";
         $stmt1 = $pdo->prepare($sqlcheck);
-        $stmt1->execute(['email' => $email, 'invite_code' => $code]);
+        $stmt1->execute(['email' => $email, 'code' => $code]);
         $stmt1_Result=$stmt1->rowCount();
+        $row = $stmt1->fetch();
+        $user_id = $row['id'];
         //echo var_dump($stmt);
         //exit;
         if($stmt1) {
-        $sql = "UPDATE users 
-                SET password= :password, photo=:photo 
+        $sql = "UPDATE user 
+                SET password= :password, photo=:photo, invite_code = NULL
                 WHERE invite_code = :code AND email = :email";
             $stmt = $pdo->prepare($sql);
-            $stmt->execute(['password'=> $password, 'photo' => $photo]);
+            $stmt->execute(['password'=> $password, 'photo' => $photo, 'code' => $code, 'email' => $email]);
+
         }
         else{
             $notification ['db_error'] = "The code and or email you provided do not match the values in the database. Please Try Again!";
@@ -120,28 +214,21 @@ if (isset($_POST['createAccount-btn'])) {
         }
 
 
-    //exit;
-    
-    //Check for errors before writing to database
-    //if (count($errors === 0)){
-        //$password = password_hash($password, PASSWORD_DEFAULT);
-        //$verified = FALSE;
+    if ($stmt) {
+        //Update Session Variables
+        $updateSession ="SELECT * FROM user WHERE id = :user_id";
+        $updateSessionResult = $pdo->prepare($updateSession);
+        $updateSessionResult->execute(['user_id' => $user_id]);
+        $row= $updateSessionResult->fetch();
+        $_SESSION ['id'] = $row['id'];
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['password'] = $row['password'];
+        $_SESSION['photo'] = $row['photo'];
+        header('Location:/onboarding/step2.php/');
 
-       // $sql = "UPDATE users SET password='$_POST[passowrd]' WHERE invite_code = $_POST[code] AND email = $_POST[email]";
-        //$stmt = $dbconnect->prepare($sql);
-       // $stmt->bind_param('s', $password);
-       // $stmt->execute();
-
-    //if ($stmt->execute()) {
-        //Login user 
-        //$user_id = $dbconnect->insert_id;
-        //$_SESSION ['id'] = $user_id;
-        //$_SESSION['email'] = $email;
-        //$_SESSION['password'] = $password;
-
-    //} else {
+    } else {
         //$errors['db_error'] = "Database error: Failed to Register";
-    //}
+    }
     }
 
 
