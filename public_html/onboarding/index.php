db5ee349f7accf8e6246032e0e6e1e3fc7e07b2f
Josh Hannon <joshhannon15@gmail.com>
2020-07-23 14:35:40 -0400

    Update index.php

--- public_html/onboarding/index.php
+++ public_html/onboarding/index.php
@@ -5,27 +5,27 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
 
  //Authorization System
 //Secure resource: invited user with email and valid code only
-if (!isset($_GET["email"]) && !isset($_GET["code"])) {
-    header("HTTP/1.1 401 Unauthorized");
+//if (!isset($_GET["email"]) && !isset($_GET["code"])) {
+    //header("HTTP/1.1 401 Unauthorized");
     // An error page
-    header("Location: error401.php");
-    exit;
-}
+    //header("Location: error401.php");
+    //exit;
+//}
 
-$email = $_GET["email"];
-$code = $_GET["code"];
+//$email = $_GET["email"];
+//$code = $_GET["code"];
 
-$query = "SELECT * FROM user WHERE invite_code = :invite_code";
+//$query = "SELECT * FROM user WHERE invite_code = :invite_code";
 
-$invite_code = $pdo->prepare($query);
-$invite_code->execute(['invite_code' => $code]);
+//$invite_code = $pdo->prepare($query);
+//$invite_code->execute(['invite_code' => $code]);
 
-if ($invite_code->rowCount() != 1) {
-    header("HTTP/1.1 401 Unauthorized");
+//if ($invite_code->rowCount() != 1) {
+    //header("HTTP/1.1 401 Unauthorized");
     // An error page
-    header("Location: error401.php");
-    exit;
-}
+    //header("Location: error401.php");
+    //exit;
+//}
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
  
 // turn on output buffering
@@ -133,7 +133,6 @@ if (isset($_POST['createAccount-btn'])) {
 ?>
 
 {header}
-{main_nav}
 <h1>Sign Up</h1>
   <div>  
             <form action="<?php echo $_SERVER["PHP_SELF"]; ?>"  method="post">
@@ -151,7 +150,7 @@ if (isset($_POST['createAccount-btn'])) {
                 </div>
                     <input type="submit" class="btn btn-primary" name="createAccount-btn" value="Create Account"></input>
             </form>
-    
+            <p>Already have a Account? <a href="/login/">Login</a></p>
     
     
     </div>
