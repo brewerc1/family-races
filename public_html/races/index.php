c0d0fcf8f799038b7f62367d438bfe89b462f729
kenryumae <ken.ryumae@gmail.com>
2020-07-30 11:37:50 -0400

    Updated All Races to now properly show total prize
    All Races now show the total prize money won up to
    the current event.
    
    Fixed bug where error would pop up when viewing
    an old event's all races tab.

--- public_html/races/index.php
+++ public_html/races/index.php
@@ -47,16 +47,22 @@ $uid = $_SESSION['id'];
 
 // URL needs to have the GET variables to work ex: http://localhost/races/?e=1&r=3
 // Handle Event
-if(!empty($_GET['e']) && is_numeric($_GET['e'])){
+if (!empty($_GET['e']) && is_numeric($_GET['e'])) {
     $event = $_GET['e'];
-}else{
+}
+else {
     $event = $_SESSION['current_event'];
 }
 
 // Handle Race TODO: impliment $_SESSION['current_race']
-if(!empty($_GET['r']) && is_numeric($_GET['r'])){
+
+if (!empty($_GET['r']) && is_numeric($_GET['r'])){
     $race = $_GET['r'];
-}else{
+}
+elseif (!empty($_GET['r']) && $_GET['r'] == 'all') {
+    $race = 0;
+}
+else {
     $current_race_sql = "SELECT * FROM `race` WHERE race.event_id = :event AND race.window_closed = 0 LIMIT 1";
     $current_race_result = $pdo->prepare($current_race_sql);
     $current_race_result->execute(['event' => $event]);
@@ -78,7 +84,7 @@ if($_SESSION['site_memorial_race_enable'] == '1'){
 }
 
 ///// DEBUG
-$debug = debug("UID: $uid<br>Event: $event");
+$debug = debug("UID: $uid<br>Event: $event<br>Race: $race");
 ///// end DEBUG
 
 // Gather data for this page
@@ -124,6 +130,30 @@ $race_standings_result = $pdo->prepare($race_standings_sql);
 $race_standings_result->execute(['event' => $event, 'race' => $race, 'user_id' => $uid]);
 $race_standings_info = $race_standings_result->fetch();
 
+// SQL to get the event standings
+// SELECT user_id, SUM(earnings) total FROM `race_standings` WHERE race_standings.race_event_id = 1 GROUP BY race_standings.user_id ORDER BY total DESC
+// SELECT user.first_name, user.last_name, user_id, SUM(earnings) total FROM `race_standings` INNER JOIN `user` ON user.id=race_standings.user_id WHERE race_standings.race_event_id = 1 GROUP BY race_standings.user_id ORDER BY total DESC
+$event_standings_sql = "SELECT user.first_name, user.last_name, user.photo, user.update_time, user_id, SUM(earnings) total FROM `race_standings` INNER JOIN `user` ON user.id=race_standings.user_id WHERE race_standings.race_event_id = :event GROUP BY race_standings.user_id ORDER BY total DESC";
+$event_standings_result = $pdo->prepare($event_standings_sql);
+$event_standings_result->execute(['event' => $event]);
+$user_count = $event_standings_result->rowCount();
+
+// SQL Queries to get the win, place, show horses
+$win_horse_sql = "SELECT * FROM `horse` WHERE horse.race_event_id = :event AND horse.race_race_number = :race AND horse.finish = 'win' LIMIT 1";
+$win_horse_result = $pdo->prepare($win_horse_sql);
+$win_horse_result->execute(['event' => $event, 'race' => $race]);
+$win_horse = $win_horse_result->fetch();
+
+$place_horse_sql = "SELECT * FROM `horse` WHERE horse.race_event_id = :event AND horse.race_race_number = :race AND horse.finish = 'place' LIMIT 1";
+$place_horse_result = $pdo->prepare($place_horse_sql);
+$place_horse_result->execute(['event' => $event, 'race' => $race]);
+$place_horse = $place_horse_result->fetch();
+
+$show_horse_sql = "SELECT * FROM `horse` WHERE horse.race_event_id = :event AND horse.race_race_number = :race AND horse.finish = 'show' LIMIT 1";
+$show_horse_result = $pdo->prepare($show_horse_sql);
+$show_horse_result->execute(['event' => $event, 'race' => $race]);
+$show_horse = $show_horse_result->fetch();
+
 $background_image = random_photo();
 
 ?>
@@ -131,94 +161,115 @@ $background_image = random_photo();
 {main_nav}
 <main role="main" id="races_page">
 
+    <?php
+        if ($memorial_race_number == $race) {
+            echo <<< MEMORIAL
+            <div class="memorial-banner">
+            </div>
+MEMORIAL;
+        }?>
+
     <div class="card" style="background-image: url(<?php echo $background_image['filename'];?>);">
         <div class="input-group input-group-lg mb-3 pt-2 pl-2 pr-2">
             <div class="input-group-prepend">
                 <label class="input-group-text" for="race_picker">Race</label>
             </div>
             <select class="custom-select" id="race_picker">
-                <?php 
-                    // Builds the select menu based on number of races
-                    // TODO: "all" option to display event standings
-                    for($i = 1; $i <= $num_races; $i++){
-                        if($i == $race){
-                            $attr = "selected='selected' disabled='disabled'";
-                        }else{
-                            $attr = "";
-                        }
-
-                        if ($memorial_race_number == $i) {
-                            echo "<option value='e=$event&r=$i&u=$uid' $attr>" . $_SESSION['site_memorial_race_name'] . "</option>";
+                <?php
+                    for($i = 1; $i <= $num_races; $i++) {
+                        if ($i == $race) {
+                            if ($memorial_race_number == $i) {
+                                echo "<option value='e=$event&r=$i&u=$uid' selected disabled>" . $_SESSION['site_memorial_race_name'] . "</option>";
+                            }
+                            else {
+                                echo "<option value='e=$event&r=$i&u=$uid' selected disabled>Race $i</option>";
+                            }
                         }
                         else {
-                            echo "<option value='e=$event&r=$i&u=$uid' $attr>Race $i</option>";
+                            if ($memorial_race_number == $i) {
+                                echo "<option value='e=$event&r=$i&u=$uid'>" . $_SESSION['site_memorial_race_name'] . "</option>";
+                            }
+                            else {
+                                echo "<option value='e=$event&r=$i&u=$uid'>Race $i</option>";
+                            }
                         }
                     }
+                    if ($race == 0) {
+                        echo "<option value='e=$event&r=all&u=$uid' selected disabled>All Races</option>";
+                    }
+                    else {
+                        echo "<option value='e=$event&r=all&u=$uid'>All Races</option>";
+                    }
                 ?>
-                <option value="e=$event&r=0&u=$uid">All Races</option>
             </select>
         </div>
-        <?php if($race_info['window_closed'] == '0') {?>
-            <form action="bets.php" method="POST">
-                <div class="card-body">
-                    <div class="form-group input-group input-group-lg mb-3">
-                        <div class="input-group-prepend">
-                            <label class="input-group-text" for="horseSelection">Pick</label>
+        <?php if ($race_info) { // Checks to see if there is race info for the race (used to check 'All Races')
+            if($race_info['window_closed'] == '0') {?>
+                <form action="bets.php" method="POST">
+                    <div class="card-body">
+                        <div class="form-group input-group input-group-lg mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="horseSelection">Pick</label>
+                            </div>
+                            <select class="custom-select" id="horseSelection" name="horseSelection" required>
+                                <?php
+                                    // If no horse has been selected, default to 'Horse'
+                                    echo "<option value='default' selected disabled>Horse...</option>";
+                                    for($i = 0; $i < $horses_count; $i++){
+                                        if (($horse['horse_number'] == $pick['horse_number']) || ($horse['horse_number'] == $old_pick)) {
+                                            echo "<option selected>" . $horse['horse_number']. "</option>";
+                                        }
+                                        else {
+                                            echo "<option>" . $horse['horse_number'] . "</option>";
+                                        }
+                                        $horse = $horses_result->fetch();
+                                    }
+                                ?>
+                            </select>
                         </div>
-                        <select class="custom-select" id="horseSelection" name="horseSelection" required>
-                            <?php
-                                // If no horse has been selected, default to 'Horse'
-                                echo "<option value='default' selected disabled>Horse...</option>";
-                                for($i = 0; $i < $horses_count; $i++){
-                                    if (($horse['horse_number'] == $pick['horse_number']) || ($horse['horse_number'] == $old_pick)) {
-                                        echo "<option selected>" . $horse['horse_number']. "</option>";
-                                    }
-                                    else {
-                                        echo "<option>" . $horse['horse_number'] . "</option>";
-                                    }
-                                    $horse = $horses_result->fetch();
+                        <div class="form-group input-group input-group-lg mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="placeSelection">Finish</label>
+                            </div>
+                            <select class="custom-select" id="placeSelection" name="placeSelection" required>
+                                <!-- If no finish has been selected, default to 'Choose' -->
+                                <option value="default" selected disabled>Choose...</option>
+                                <?php 
+                                if (($pick['finish'] == 'win') || ($old_finish == 'win')) {
+                                    echo "<option value='win' selected>Win</option>";
                                 }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="form-group input-group input-group-lg mb-3">
-                        <div class="input-group-prepend">
-                            <label class="input-group-text" for="placeSelection">Finish</label>
+                                else {
+                                    echo "<option value='win'>Win</option>";
+                                }
+                                if (($pick['finish'] == 'place') || ($old_finish == 'place')) {
+                                    echo "<option value='place' selected>Place</option>";
+                                }
+                                else {
+                                    echo "<option value='place'>Place</option>";
+                                }
+                                if (($pick['finish'] == 'show') || ($old_finish == 'show')) {
+                                    echo "<option value='show' selected>Show</option>";
+                                }
+                                else {
+                                    echo "<option value='show'>Show</option>";
+                                }
+                                ?>
+                            </select>
                         </div>
-                        <select class="custom-select" id="placeSelection" name="placeSelection" required>
-                            <!-- If no finish has been selected, default to 'Choose' -->
-                            <option value="default" selected disabled>Choose...</option>
-                            <?php 
-                            if (($pick['finish'] == 'win') || ($old_finish == 'win')) {
-                                echo "<option value='win' selected>Win</option>";
-                            }
-                            else {
-                                echo "<option value='win'>Win</option>";
-                            }
-                            if (($pick['finish'] == 'place') || ($old_finish == 'place')) {
-                                echo "<option value='place' selected>Place</option>";
-                            }
-                            else {
-                                echo "<option value='place'>Place</option>";
-                            }
-                            if (($pick['finish'] == 'show') || ($old_finish == 'show')) {
-                                echo "<option value='show' selected>Show</option>";
-                            }
-                            else {
-                                echo "<option value='show'>Show</option>";
-                            }
-                            ?>
-                        </select>
+                        <input type="hidden" value=<?php echo "$race"; ?> name="currentRace" id="currentRace">
+                        <input class="btn btn-primary" type="submit" value="Submit">
                     </div>
-                    <input type="hidden" value=<?php echo "$race"; ?> name="currentRace" id="currentRace">
-                    <input class="btn btn-primary" type="submit" value="Submit">
-                </div>
-            </form>
+                </form>
         <?php } 
         elseif ($race_info['window_closed'] == '1') {?>
             <div class="card-body" id="window_closed">
-                <div class="card-meta">The window for this race is closed</div>
-                <?php if($pick){ ?>
+                <?php if ($win_horse) {
+                        echo "<div class='card-meta'></div>";
+                    }
+                    else {
+                        echo "<div class='card-meta'>The betting window is closed. Awaiting race results</div>";
+                    }
+                    if($pick){ ?>
                     <div class="card-text" id="your_bet">
                         <h1>You picked horse #<?php echo "<span class='horse-number'>{$pick['horse_number']}</span> to <span class='horse-finish'>" . ucfirst($pick['finish']) . "</span>";?></h1>
                         <?php if ($race_standings_info) {
@@ -231,20 +282,32 @@ $background_image = random_photo();
                         <!-- When there is no bet, they will not win anything, so default to $0.00 -->
                         <h3>Purse: $0.00</h3>
                     </div>
-                <?php } ?>
-            </div>
-        <?php } else { // SHOULD NEVER REACH HERE!!! ?>
-            <h1> ERROR! </h1>
-        <?php } ?>
+                <?php }
+                    if ($win_horse) {
+                        echo "<hr/>";
+                        echo "<h2>Race Results</h2>";
+                        echo "<h3>Win: Horse #{$win_horse['horse_number']}</h3>";
+                        echo "<h4>\${$win_horse['win_purse']} | \${$win_horse['place_purse']} | \${$win_horse['show_purse']}</h4>";
+                        echo "<h3>Place: Horse #{$win_horse['horse_number']}</h3>";
+                        echo "<h4>\${$place_horse['place_purse']} | \${$place_horse['show_purse']}</h4>";
+                        echo "<h3>Show: Horse #{$win_horse['horse_number']}</h3>";
+                        echo "<h4>\${$show_horse['show_purse']}</h4>";
+                    }
+                } 
+                else { // SHOULD NEVER REACH HERE!!!
+                echo "<h1> ERROR! </h1>";
+                }
+            }
+        else { // Comes here when showing 'All Races'
+            echo "<div class='card-body' id='window_closed'><h1 class='card-title'>Current Event Leaderboard</h1></div>";
+        }?>
+        </div>
     </div>
     
 	<ul class="user-list list-group list-group-flush" id="race_leaderboard">
 <?php
-if ($num_race_results > 0) {
-    $invited = "";
-
-    // Output data of each row
-    while($row = $race_result->fetch()) {
+if ($race == 0) { // Case when we are displaying the entire leaderboard
+    while($row = $event_standings_result->fetch()) {
         $name = $row["first_name"] . ' ' . $row["last_name"];
         $update_time_stamp = strtotime($row["update_time"]); // convert to timestamp for cache-busting
         
@@ -254,6 +317,32 @@ if ($num_race_results > 0) {
             $photo = $row["photo"] . "?$update_time_stamp"; // cache-bust this image
         }
         echo <<< HERE
+    <li class="list-group-item">
+        <div class="media">
+            <a href="/user/?u={$row["user_id"]}">
+                <img src="$photo" alt="A photo of $name" class="rounded-circle">
+            </a>
+            <div class="media-body"><span class="user_name d-inline-block px-3">$name</span> <span class="earnings badge badge-success float-right px-2">\${$row["total"]}</span></div>
+        </div>
+    </li>
+HERE;
+    }
+}
+else { // Other cases when individual race results are being shown
+    if ($num_race_results > 0) {
+        $invited = "";
+
+        // Output data of each row
+        while($row = $race_result->fetch()) {
+            $name = $row["first_name"] . ' ' . $row["last_name"];
+            $update_time_stamp = strtotime($row["update_time"]); // convert to timestamp for cache-busting
+            
+            if(empty($row["photo"])) { // Handle missing profile photo
+                $photo = "/images/no-user-image.jpg"; // do not cache-bust this image
+            }else{
+                $photo = $row["photo"] . "?$update_time_stamp"; // cache-bust this image
+            }
+            echo <<< HERE
         <li class="list-group-item">
             <div class="media">
                 <a href="/user/?u={$row["user_id"]}">
@@ -263,18 +352,18 @@ if ($num_race_results > 0) {
             </div>
         </li>
 HERE;
-    }
-} else {
-    if ($race_info['window_closed'] == '0') { // Don't show any text when the window is still open
+        }
+    } else {
+        if ($race_info['window_closed'] == '0') { // Don't show any text when the window is still open
 
     }
     else { // Show there is no results entered in yet
         echo <<< NORESULTS
             <div class="no-results">
-                <div>Race results are not in. Check&nbsp;back&nbsp;later.</div>
             </div>
 NORESULTS;
-    }
+        }
+}
 }
 ?>
     </ul> <!-- END id race_leaderboard -->
