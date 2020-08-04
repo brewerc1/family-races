e6dab722dfb7677cc54446d28b2501d951c67977
Josh Hannon <joshhannon15@gmail.com>
2020-08-04 13:45:37 -0400

    Update bootstrap.php

--- public_html/bootstrap.php
+++ public_html/bootstrap.php
@@ -142,7 +142,10 @@ $messages = array(
     15 => "Settings updated.",
     16 => "User deactivated.",
     17 => "User activated.",
-    18 => "Invite deleted."
+    18 => "Invite deleted.",
+    19 => "Please fill in a first name",
+    20 => "Please fill in last name",
+    21 => "Please fill in code"
 );
 
 $notification = '';
