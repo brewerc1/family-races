e45f3983ee9f5b3b21d5aa27ab468bc93c85b9af
Josh Hannon <joshhannon15@gmail.com>
2020-08-04 16:54:41 -0400

    Update step2.php

--- public_html/onboarding/step2.php
+++ public_html/onboarding/step2.php
@@ -78,43 +78,42 @@ if (isset($_POST['next-btn'])) {
 ?>
 {header}
 {main_nav}
-<h1>Your Profile</h1>
-
-    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>"  method="post">
-        <p class="text-white">First name and last name are REQUIRED even if you choose to skip the rest.</p>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="motto" class="col-form-label text-white" >First Name:</label>
-                <input  type="text"  required class="form-control" id="first_name" name="first_name"></input>
+<main role="main" id="onboarding_page">
+    <h1 class="mb-5 sticky-top">Your Profile</h1>
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>"  method="post">
+            <p class="text-center">First name and last name are REQUIRED even if you choose to skip the rest.</p>
+            <section>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="motto" class="col-form-label" >First Name:</label>
+                        <input  type="text"  required class="form-control" id="first_name" name="first_name"></input>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="motto" class="col-form-label" >Last Name:</label>
+                        <input  type="text"  required class="form-control" id="last_name" name="last_name"></input>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="city" class="col-form-label" >City:</label>
+                        <input  type="text"  class="form-control" id="city" name="city"></input>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="state" class="col-form-label" >State:</label>
+                        <input  type="text" class="form-control" id="state" name="state"></input>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col">
+                        <label for="motto" class="col-form-label" >Motto:</label>
+                        <textarea  class="form-control" id="motto" name="motto"></textarea>
+                    </div>
+                </div>
+            </section>
+            <div class="text-center">
+                <input type="submit" class="btn btn-primary" name="next-btn" value="Next"></input>
             </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="motto" class="col-form-label text-white" >Last Name:</label>
-                <input  type="text"  required class="form-control" id="last_name" name="last_name"></input>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="city" class="col-form-label text-white" >City:</label>
-                <input  type="text"  class="form-control" id="city" name="city"></input>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="state" class="col-form-label text-white" >State:</label>
-                <input  type="text" class="form-control" id="state" name="state"></input>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="motto" class="col-form-label text-white" >Motto:</label>
-                <input  type="text" class="form-control" id="motto" name="motto"></input>
-            </div>
-        </div>
-        <div class="text-center">
-            <input type="submit" class="btn btn-primary" name="next-btn" value="Next"></input>
-        </div>
-    </form>
+        </form>
+</main>
     {footer}
 <?php ob_end_flush(); ?>
