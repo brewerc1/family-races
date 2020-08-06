d463c48fb484e2b8f6532c973ade2072cd2d7448
Chris Brewer <brewerc1@nku.edu>
2020-08-06 12:22:05 -0400

    Update close-race.php

--- public_html/races/close-race.php
+++ public_html/races/close-race.php
@@ -22,6 +22,6 @@ $race = $_POST['currentRace'];
 $close_race_sql = "UPDATE `race` SET `window_closed` = 1 WHERE `race`.`event_id` = :event AND `race`.`race_number` = :race";
 $close_race = $pdo->prepare($close_race_sql);
 $close_race->execute(['event' => $event, 'race' => $race]);
-header("Location: /races/?e={$event}&r={$race}&m=15&s=success");
+header("Location: /races/?e={$event}&r={$race}&m=22&s=success");
 
 ?> 
