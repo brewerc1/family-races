3332b35e445bb5f291d8b82ec43a7592ec71bbdc
Chris Brewer <brewerc1@nku.edu>
2020-08-12 13:42:17 -0400

    HOTFIX
    Adjust event table width

--- public_html/admin/events/index.php
+++ public_html/admin/events/index.php
@@ -27,9 +27,9 @@ try {
 			$events_result = $events_query->fetchAll();
 			foreach ($events_result as $id => $event_data){
 				$has_current_event = $events_result[$id]['status'] == 0 ? 0 : 1;
-				$output .=  "\t\t\t\t<li class='list-group-item'>
-					<a href='./manage.php?e={$events_result[$id]['id']}'>{$events_result[$id]['name']} <span class='px-2 status_badge badge badge-pill float-right badge-" . ( $events_result[$id]['status'] == 1 ? "info'>completed" : "success'>active" ) . "</span></a>
-				</li>\n";
+				$output .=  "\t\t\t\t\t<li class='list-group-item'>
+						<a href='./manage.php?e={$events_result[$id]['id']}'>{$events_result[$id]['name']} <span class='px-2 status_badge badge badge-pill float-right badge-" . ( $events_result[$id]['status'] == 1 ? "info'>completed" : "success'>current" ) . "</span></a>
+					</li>\n";
 			}
 		} else {
 			$output = '<p class="alert alert-info" role="alert">No events have been created.</p>';
@@ -53,9 +53,11 @@ try {
 				<a class="btn btn-primary text-center <?php echo $has_current_event == 1 ? 'disabled' : '' ?>" href="./create.php">Create New Event</a>
 				<?php echo $has_current_event == 1 ? '<div><small>To create a new event, you must close the current event.</small></div>' : '';?>
 			</div>
-			<ul class="list-group list-group-flush mt-3">
+			<div class="justify-content-center row mt-5">
+				<ul class="list-group list-group-flush col-md-6">
 <?php echo $output;?>
-            </ul>
+            	</ul>
+			</div>
 		</section>
     </main>
 
