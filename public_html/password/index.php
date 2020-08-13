c6ee8521a976343370201c9d0be5eac1f006ebe7
Chris Brewer <brewerc1@nku.edu>
2020-08-13 15:17:08 -0400

    Password reset fixes

--- public_html/password/index.php
+++ public_html/password/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php');
-ob_start('template');
 
+ob_start('template');
 
 // set the page title for the template
 $page_title = "Forgot Password";
@@ -11,9 +11,9 @@ $javascript = "";
 
 ?>
 {header}
-<main role="main">
+<main role="main" id="password_reset_page">
+	<h1 class="mb-5 sticky-top">Reset Your Password</h1>
     <section>
-        <h1>Recover Your Password</h1>
         <p>
             To reset your password. enter your account email address. An email will be sent with a reset code.
         </p>
@@ -25,7 +25,7 @@ $javascript = "";
             <input type="submit" name="reset_password" value="Reset Password">
         </form>
         <p>
-            <a href="http://localhost/login">Return to Log In</a>
+            <a href="/login">Return to Log In</a>
         </p>
     </section>
 </main>
