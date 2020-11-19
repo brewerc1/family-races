2146d0fe97e395b237281b7f3e738516280898fe
Chris Brewer <brewerc1@nku.edu>
2020-11-19 12:13:58 -0500

    Update index.php
    Change text in pill for an event that's been closed by admin from 'completed' to 'closed' for better consistency based on customer feedback.

--- public_html/admin/events/index.php
+++ public_html/admin/events/index.php
@@ -31,7 +31,7 @@ try {
                     $has_current_event = 1;
 
 				$output .=  "\t\t\t\t\t<li class='list-group-item'>
-						<a href='./manage.php?e={$events_result[$id]['id']}'>{$events_result[$id]['name']} <span class='px-2 status_badge badge badge-pill float-right badge-" . ( $events_result[$id]['status'] == 1 ? "info'>completed" : "success'>current" ) . "</span></a>
+						<a href='./manage.php?e={$events_result[$id]['id']}'>{$events_result[$id]['name']} <span class='px-2 status_badge badge badge-pill float-right badge-" . ( $events_result[$id]['status'] == 1 ? "info'>closed" : "success'>current" ) . "</span></a>
 					</li>\n";
 			}
 		} else {
