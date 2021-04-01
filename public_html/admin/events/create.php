0419bf2a39c27b729e75d3431c109abe8b7cc024
unknown <nicholasklaene@outlook.com>
2021-04-01 08:47:46 -0400

    fixes issue #214

--- public_html/admin/events/create.php
+++ public_html/admin/events/create.php
@@ -67,7 +67,7 @@ if (isset($_POST["submit"])) {
 								<div class="input-group-prepend">
 									 <span class="input-group-text"><i class="far fa-horse-head"></i></span>
   								</div>
-								  <input type="text" class="form-control" id="name" name="event_name">
+								  <input type="text" class="form-control" id="name" name="event_name" maxlength="25">
 							</div>
 	                    </div>
 	                	<!-- Event Date -->
