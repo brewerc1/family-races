ae2232faf98557fe097b0a9c550d7de23b3ae441
Jonathan Makunga <jmakunga2015@gmail.com>
2021-05-01 01:14:21 -0400

    Get Races api

--- public_html/api/races/index.php
+++ public_html/api/races/index.php
@@ -30,10 +30,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && validGetRequestURLParams()) {
             $query = "SELECT * FROM horse WHERE race_event_id = :race_event_id AND race_race_number = :race_race_number";
             $stmt = $pdo->prepare($query);
 
+            $horseQuery = "SELECT * FROM pick WHERE race_event_id = :race_event_id AND race_race_number = :race_race_number AND horse_number = :horse_number";
+            $horseStmt = $pdo->prepare($horseQuery);
+
             $races = array();
+            // Get horses for each race
             foreach ($raceData["races"] as $race) {
                 $stmt->execute(["race_event_id" => $race["event_id"], "race_race_number" => $race["race_number"]]);
-                $race["horses"] = $stmt->fetchAll();
+                $horses = $stmt->fetchAll();
+                $horsesVal = array();
+                // Answer: Whether a horse can be deleted
+                foreach ($horses as $horse) {
+                    $horseStmt->execute(["race_event_id" => $horse["race_event_id"],
+                        "race_race_number" => $horse["race_race_number"], "horse_number" => $horse["horse_number"]]);
+
+                    if ($horseStmt->rowCount() > 0) {
+                        $horse["can_be_delete"] = false;
+                    } else {
+                        $horse["can_be_delete"] = true;
+                    }
+                    $horsesVal[] = $horse;
+                }
+
+                $race["horses"] = $horsesVal;
                 $races[] = $race;
             }
 
